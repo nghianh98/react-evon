b5627c439621ff9084df1ea8ba541a9cb22a07ea
nghianh98 <nghianh.0811@gmail.com>
2022-03-18 08:31:14 +0700

    done tictactoe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import YoutubeList from "./components/youtube/YoutubeList";
 import Toggle from "./components/toggle/Toggle";
 import Game from "./components/tictactoe/Game";
 import "./App.css";
+import GameWithReducer from "./components/tictactoe/GameWithReducer";
 
 function App() {
   const name = "nghianh98";
@@ -11,6 +12,7 @@ function App() {
       <YoutubeList>{name} render using props.children</YoutubeList>
       <Toggle></Toggle>
       <Game></Game>
+      <GameWithReducer></GameWithReducer>
     </div>
   );
 }
